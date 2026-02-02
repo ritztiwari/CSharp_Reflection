862ef9c19cb3563fb61b2f112274345f45c03561
Rishabh Tiwari <mrishabh532@gmail.com>
2026-02-02 11:08:01 +0530

    [Rishabh]. Adding Program.cs to run all the practice problems in reflection project.

---
+++ Reflection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CSharp_Reflection.Reflection.GetInformation;
+using CSharp_Reflection.Reflection.AccessPrivateFields;
+using CSharp_Reflection.Reflection.InvokePrivateMethod;
+using CSharp_Reflection.Reflection.DynamicObjectCreation;
+using CSharp_Reflection.Reflection.DynamicMethodInvocation;
+using CSharp_Reflection.Reflection.UpdateApiKey;
+using CSharp_Reflection.Reflection.RetrieveAttributeAtRuntime;
+using CSharp_Reflection.Reflection.MethodExecutionTiming;
+using CSharp_Reflection.Reflection.GenerateAJSON;
+
+namespace Name
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // GetInfo.Get();
+
+            // AccessField.Get();
+
+            // InvokeMethod.Execute();
+
+            // Create.Instance();
+
+            // InvokeMethods.Invoke();
+
+            // Update.Execute();
+
+            // RetrieveAttributeExecute.Execute();
+
+            // TimeMethodExecution.Execute();
+
+            Generate.Execute();
+        }
+    }
+}
